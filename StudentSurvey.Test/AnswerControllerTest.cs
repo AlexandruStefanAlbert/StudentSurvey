3c548f8677e95bf25434e6ecd5e9d80d3ad42095
AlexandruStefanAlbert <stefan-albert.alexandru@student.tuiasi.ro>
2022-08-01 12:51:11 +0300

    Test

---
+++ StudentSurvey.Test/AnswerControllerTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyHotel.Domain.Entities;
+using StudentSurvey.Business.Models;
+
+namespace StudentSurvey.Test
+{
+    [TestClass]
+    public class AnswerControllerTest : BaseIntegrationTests
+    {
+
+        /*[TestMethod]
+        public async Task When_AddAnswer_ShouldAddAnswer()
+        {
+            var Addanswer = new AnswerModel
+            {
+                QuestionID = 12,
+                Option1 = "Avem",
+                Option2 = "Bbe",
+                Option3 = "Cer", 
+                Option4 = "Dor",
+                
+            };
+            var result = await HttpClient.PostAsJsonAsync("api/Answer", Addanswer);
+            //Assert
+            result.EnsureSuccessStatusCode();
+            var AnswerIdfromresult = await result.Content.ReadAsStringAsync();
+            AnswerIdfromresult.Should().NotBeNullOrEmpty();
+            var ResultofGetAnswerByID = await HttpClient.GetAsync($"api/Answer/{AnswerIdfromresult}");
+            ResultofGetAnswerByID.EnsureSuccessStatusCode();
+            var AnswerfromResults = await ResultofGetAnswerByID.Content.ReadFromJsonAsync<Answers>();
+            AnswerfromResults.Should().NotBeNull();
+            AnswerfromResults.Id.ToString().Should().Be(AnswerIdfromresult);
+
+        }
+
+        [TestMethod]
+        public async Task When_UpdateAnswer_ShouldChangeAnswerData()
+        {
+
+
+            var Addanswer = new AnswerModel
+            {
+                QuestionID = 1,
+                Option1 = "Are",
+                Option2 = "Bere",
+                Option3 = "Cere",
+                Option4 = "Doare"
+            };
+            var result = await HttpClient.PostAsJsonAsync("api/Answer", Addanswer);
+            //Assert
+            result.EnsureSuccessStatusCode();
+            var AnswerIdfromresult = await result.Content.ReadAsStringAsync();
+
+            var expectedDataChangeAnswer = "Merge";
+            var answer = new AnswerModel
+            {
+                Id = Convert.ToInt32(AnswerIdfromresult),
+                QuestionID = 1,
+                Option1 = "Are",
+                Option2 = "Bere",
+                Option3 = "Cere",
+                Option4 = expectedDataChangeAnswer
+            };
+            var resultforUpdateAnswer = await HttpClient.PutAsJsonAsync("api/Answer", answer);
+            //Assert
+
+            resultforUpdateAnswer.EnsureSuccessStatusCode();
+
+            var ResultofGetAnswerByID = await HttpClient.GetAsync($"api/Answer/{AnswerIdfromresult}");
+            ResultofGetAnswerByID.EnsureSuccessStatusCode();
+            var AnswerfromResults = await ResultofGetAnswerByID.Content.ReadFromJsonAsync<Answers>();
+            AnswerfromResults.Should().NotBeNull();
+            AnswerfromResults.Id.ToString().Should().Be(AnswerIdfromresult);
+            AnswerfromResults.Option4.Should().Be(expectedDataChangeAnswer);
+
+
+        }*/
+
+        [TestMethod]
+        public async Task When_GetAll_ShouldGiveOKResponse()
+        {
+
+
+
+            var response = await HttpClient.GetAsync("/api/Answer");
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+    }
+}
---
+++ StudentSurvey.Test/ConsentControllerTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyHotel.Domain.Entities;
+using StudentSurvey.Business.Models;
+
+namespace StudentSurvey.Test
+{
+    [TestClass]
+    public class ConsentControllerTest : BaseIntegrationTests
+    {
+
+        /*[TestMethod]
+        public async Task When_AddConsent_ShouldAddConsent()
+        {
+           
+            var Addconsent = new ConsentModel
+            {
+                UserID = 1,
+                SurveyID = 1,
+                ExpirationDate = DateTime.Now.AddMonths(36)
+
+            };
+            var result = await HttpClient.PostAsJsonAsync("api/Consent", Addconsent);
+            //Assert
+            result.EnsureSuccessStatusCode();
+            var ConsentIdfromresult = await result.Content.ReadAsStringAsync();
+            ConsentIdfromresult.Should().NotBeNullOrEmpty();
+            var ResultofGetConsentByID = await HttpClient.GetAsync($"api/Consent/{ConsentIdfromresult}");
+            ResultofGetConsentByID.EnsureSuccessStatusCode();
+            var ConsentfromResults = await ResultofGetConsentByID.Content.ReadFromJsonAsync<Consent>();
+            ConsentfromResults.Should().NotBeNull();
+            ConsentfromResults.Id.ToString().Should().Be(ConsentIdfromresult);
+
+        }*/
+
+       /* [TestMethod]
+        public async Task When_UpdateConsent_ShouldChangeConsentData()
+        {
+
+
+            var Addconsent = new ConsentModel
+            {
+                UserID = 1,
+                SurveyID = 1,
+                ExpirationDate = DateTime.Now.AddMonths(36),
+                Agree=true
+            };
+            var result = await HttpClient.PostAsJsonAsync("api/Consent", Addconsent);
+            //Assert
+            result.EnsureSuccessStatusCode();
+            var ConsentIdfromresult = await result.Content.ReadAsStringAsync();
+
+            var expectedDataChangeConsent = false;
+            var consent = new ConsentModel
+            {
+                Id = Convert.ToInt32(ConsentIdfromresult),
+                UserID = 1,
+                SurveyID = 1,
+                ExpirationDate = DateTime.Now.AddMonths(36),
+                Agree = expectedDataChangeConsent
+            };
+            var resultforUpdateConsent = await HttpClient.PutAsJsonAsync("api/Consent", consent);
+            //Assert
+
+            resultforUpdateConsent.EnsureSuccessStatusCode();
+
+            var ResultofGetConsentByID = await HttpClient.GetAsync($"api/Consent/{ConsentIdfromresult}");
+            ResultofGetConsentByID.EnsureSuccessStatusCode();
+            var ConsentfromResults = await ResultofGetConsentByID.Content.ReadFromJsonAsync<Consent>();
+            ConsentfromResults.Should().NotBeNull();
+            ConsentfromResults.Id.ToString().Should().Be(ConsentIdfromresult);
+            ConsentfromResults.Agree.Should().Be(expectedDataChangeConsent);
+
+
+        }*/
+
+        [TestMethod]
+        public async Task When_GetAll_ShouldGiveOKResponse()
+        {
+
+
+
+            var response = await HttpClient.GetAsync("/api/Consent");
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+    }
+}
